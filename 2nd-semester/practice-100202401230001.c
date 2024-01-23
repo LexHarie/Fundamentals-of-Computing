8195069e932bc32c48fd6b7ebc25be71397c8326
Lex Harie Pisco <lex.pisco@whitecloak.com>
2024-01-23 23:49:07 +0800

    feat: 2024-01-23 practice

--- 2nd-semester/practice-100202401230001.c
+++ 2nd-semester/practice-100202401230001.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <string.h>
+
+// how to print a sentence in C with scanf
 
 int main()
 {
-  printf("%d", printf("%s", "Hello Wor2ld!"));
+  char ch;
+  scanf(" %c", &ch); // to note the space before %c to consume the newline
+  printf("%c\n", ch);
+
+  char s[100];
+  scanf("%s", s);
+  printf("%s\n", s);
+
+  char sen[10000];
+  getchar(); // Consume the newline character left in the buffer
+  // it will still work without the getchar
+  fgets(sen, sizeof(sen), stdin);
+  printf("%s", sen);
   return 0;
 }
